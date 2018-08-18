1b4ca7f2424d0ff3ea45a99e16c75108bba2470d
Johannes Schuldt-Jensen <johannesschuldt-jensen@Johanness-MBP.lan>
2018-08-18 17:27:41 +0200

    now creates and sorts some values based on some other stuff

--- scraping_hacker_news.py
+++ scraping_hacker_news.py
@@ -2,19 +2,21 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-url = "https://news.ycombinator.com/newest"
 
-html = requests.get(url)
-soup = BeautifulSoup(html.content, 'html.parser')
 
 
 class article:
     def __init__(self, title, url):
         self.title = title
         self.url = url
+        self.rank = 0
 
 
-def title_lister (soup):
+def title_lister ():
+    url = "https://news.ycombinator.com/newest"
+    html = requests.get(url)
+    soup = BeautifulSoup(html.content, 'html.parser')
+
     articles = []
     result = soup.findAll('td', attrs = {'class' : "title"})
     pattern = re.compile(r"\(\w*\.\w*\)")
@@ -22,7 +24,7 @@ def title_lister (soup):
 
     #result is cleaned of filth! FILTH!
     for r in result:
-        title = r.get_text()[:re.search(pattern, r.get_text()).span()[0]].strip()
+        title = r.get_text()[:re.search(pattern, r.get_text()).span()[0]].strip().capitalize()
         href = r.find('a').get('href')
         articles.append(article(title,href))
 
