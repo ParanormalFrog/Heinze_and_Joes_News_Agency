52d5d9a159274075f46b053bcfad099a872915e9
Johannes Schuldt-Jensen <johannesschuldt-jensen@Johanness-MBP.lan>
2018-08-18 17:42:26 +0200

    made a function now. It works

--- headline_ranker.py
+++ headline_ranker.py
@@ -2,19 +2,18 @@
 import scraping_hacker_news
 import random
 
-articles = []
 
+def rank_and_drop():
+    articles = []
 
+    articles.extend(scraping_hacker_news.title_lister())
+    word_list = set(['AI', 'Artificial Intelligence', 'competitive', 'AGI', 'Machine Learning', "SQL" 'ML', 'Post-Modernism', 'Post Modernism', 'UBI', 'Universal Basic Income', 'python', 'py', 'programming'])
 
-articles.extend(scraping_hacker_news.title_lister())
-word_list = set(['AI', 'Artificial Intelligence', 'competitive', 'AGI', 'Machine Learning', "SQL" 'ML', 'Post-Modernism', 'Post Modernism', 'UBI', 'Universal Basic Income', 'python', 'programming'])
+    for article in articles:
+        words = article.title.split()
+        for word in words:
+            article.rank += (word in word_list)
 
-for article in articles:
-    words = article.title.split()
-    for word in words:
-        article.rank += (word in word_list)
-
-articles = sorted(articles, key=lambda article : article.rank, reverse = True)
-
-print([article.rank for article in articles])
-print([article.title for article in articles])
+    articles = sorted(articles, key=lambda article : article.rank, reverse = True)
+    articles = [article for article in articles if aritcle.rank > 0]
+    return articles
