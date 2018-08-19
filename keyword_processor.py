10bbd46243d8c98865eff53bd11c2a220ee1b9b5
Johannes Schuldt-Jensen <johannesschuldt-jensen@Johanness-MBP.lan>
2018-08-19 13:44:05 +0200

    Now writes keyword to a textfile in the same  directory

--- keyword_processor.py
+++ keyword_processor.py
@@ -1,11 +1,15 @@
 # Takes in a keyword and turns it into a regular expression
-
+import os
 import sys
 import re
+
+file = open(os.getcwd()+"/test.file.txt", mode='a+')
 
 
 def generaliser():
     keyword = sys.argv[1:]
     keyword = " ".join(keyword).strip().upper()
-    keyword = re.compile(r"" + str(keyword) + "([\'sS]{0,2})")
+    keyword = "\n" + str(keyword) + "([\'sS]{0,2})"
     return keyword
+
+file.write(generaliser())
