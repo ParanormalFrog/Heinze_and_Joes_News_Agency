748f9906b7f6dfd7c4e165afc72ac3cfa67c84a1
Johannes Schuldt-Jensen <johannesschuldt-jensen@Johanness-MBP.lan>
2018-08-19 12:08:52 +0200

    Removed redundant print calls and cleaned up the code a bit

--- headline_ranker.py
+++ headline_ranker.py
@@ -1,19 +1,22 @@
-## some logic that imports search keywords ##
 import scraping_hacker_news
-import random
 
 
 def rank_and_drop():
     articles = []
 
     articles.extend(scraping_hacker_news.title_lister())
-    word_list = set(['AI', 'Artificial Intelligence', 'competitive', 'AGI', 'Machine Learning', "SQL" 'ML', 'Post-Modernism', 'Post Modernism', 'UBI', 'Universal Basic Income', 'python', 'py', 'programming'])
+
+    # Method needs to be defined here. Word list
+    # should come from outside function
+
+    word_list = [word.capitalize() for word in ['AI', 'Artificial Intelligence', 'Elon', 'competitive', 'AGI', 'Machine Learning', "SQL" 'ML', 'Post-Modernism', 'Post Modernism', 'UBI', 'Universal Basic Income', 'python', 'py', 'programming']]
+    word_list = set(word_list)
 
     for article in articles:
         words = article.title.split()
         for word in words:
-            article.rank += (word in word_list)
-
-    articles = sorted(articles, key=lambda article : article.rank, reverse = True)
-    articles = [article for article in articles if aritcle.rank > 0]
+            article.rank += (word.capitalize() in word_list)
+    articles = sorted(articles, key=lambda article:
+                      article.rank, reverse=True)
+    articles = [article for article in articles if article.rank > 0]
     return articles
--- scraping_hacker_news.py
+++ scraping_hacker_news.py
@@ -3,8 +3,6 @@ import requests
 import re
 
 
-
-
 class article:
     def __init__(self, title, url):
         self.title = title
@@ -12,20 +10,20 @@ class article:
         self.rank = 0
 
 
-def title_lister ():
+def title_lister():
     url = "https://news.ycombinator.com/newest"
     html = requests.get(url)
     soup = BeautifulSoup(html.content, 'html.parser')
 
     articles = []
-    result = soup.findAll('td', attrs = {'class' : "title"})
+    result = soup.findAll('td', attrs={'class': "title"})
     pattern = re.compile(r"\(\w*\.\w*\)")
     result = [something for something in result if re.search(pattern, something.get_text()) is not None]
 
-    #result is cleaned of filth! FILTH!
+    # result is cleaned of filth!
     for r in result:
         title = r.get_text()[:re.search(pattern, r.get_text()).span()[0]].strip().capitalize()
         href = r.find('a').get('href')
-        articles.append(article(title,href))
+        articles.append(article(title, href))
 
     return articles
